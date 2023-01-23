1efa0d102762b5706629a95e4fb85af6c7969281
Alysia Dillon <alysia.dillon@outlook.com>
2023-01-23 23:16:55 +0000

    updated tip amount to represent a percentage vs a flat dollar amount

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ print('Welcome to the Tip Calculator')
 total_bill = input('How much is the total bill? ')
 split_group = input('How many people are splitting the bill? ')
 share_tip = input('Are you all sharing the tip amount? ')
-tip_amount = input('How much is the total tip from the group? ')
+tip_amount = input('What should to tip percentage be? ')
+
+tip_amount = (float(tip_amount) / 100) + 1
 
 if share_tip == "no" or share_tip == "No":
   split_amount = float(total_bill) / int(split_group)
 elif share_tip == "yes" or share_tip == "Yes":
-  split_amount = (float(total_bill) + float(tip_amount)) / int(split_group)  
+  split_amount = (float(total_bill) * float(tip_amount)) / int(split_group)  
 
 print('Everyone should pay: $' + str(split_amount))
