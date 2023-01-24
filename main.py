bb791cab667fe6306fe50ecefa16c8e4ac8674e3
Alysia Dillon <alysia.dillon@outlook.com>
2023-01-24 20:49:47 +0000

    updated the split_amount to always display 2 decimal places and updated the last print() to format a placeholder of split_amount in the string

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 print('Welcome to the Tip Calculator')
 
-total_bill = input('How much is the total bill? ')
+total_bill = input('How much is the total bill? $')
 split_group = input('How many people are splitting the bill? ')
 share_tip = input('Are you all sharing the tip amount? ')
-tip_amount = input('What should to tip percentage be? ')
-
-tip_amount = (float(tip_amount) / 100) + 1
 
 if share_tip == "no" or share_tip == "No":
   split_amount = float(total_bill) / int(split_group)
 elif share_tip == "yes" or share_tip == "Yes":
-  split_amount = (float(total_bill) * float(tip_amount)) / int(split_group)  
+  tip_amount = input('What should to tip percentage be? ')
+  tip_amount = (float(tip_amount) / 100) + 1
+  split_amount = (float(total_bill) * float(tip_amount)) / int(split_group)
 
-print('Everyone should pay: $' + str(split_amount))
+split_amount = "{:.2f}".format(split_amount)
+
+print(f'Everyone should pay: ${split_amount}')
